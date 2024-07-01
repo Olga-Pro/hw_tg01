ff0e86b6a0cb9e129f55b74308dee3f593ab3456
Olga-Pro <oip_@inbox.ru>
2024-07-01 12:30:07 +0300

    tg02

--- hw_tg02.py
+++ hw_tg02.py
@@ -2,9 +2,11 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from aiogram import types
-from aiogram.types import InputFile
-from googletrans import Translator
+from aiogram.types import Message, FSInputFile
 from gtts import gTTS
+
+from aiogram.filters import CommandStart, Command
+from googletrans import Translator
 
 
 import os
@@ -17,14 +19,28 @@ TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 # Создание экземпляра бота
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
 translator = Translator()
 
 # Создаем папку для изображений, если она не существует
 if not os.path.exists('img'):
     os.makedirs('img')
 
-@dp.message(content_types=['photo'])
-async def handle_photos(message: types.Message):
+
+@dp.message(Command(commands=['help']))
+async def help_mess(message: Message):
+    await message.answer('Этот бот умеет выполнять команды:\n/start\n/photo\n/voice\n/text\n/help')
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Привет! Я - бот!")
+
+@dp.message(Command(commands=['photo']))
+async def request_photo(message: Message):
+    await message.answer("Пожалуйста, отправьте мне фото")
+@dp.message((lambda message: 'photo' in message.content_type))
+async def handle_photos(message: Message):
     photo = message.photo[-1]  # Берем самое большое разрешение фото
     photo_id = photo.file_id
     file = await bot.get_file(photo_id)
@@ -33,18 +49,37 @@ async def handle_photos(message: types.Message):
     await bot.download_file(file_path, destination)
     await message.reply("Фото сохранено!")
 
-@dp.message(commands=['voice'])
+@dp.message(Command(commands='voice'))
 async def send_voice(message: types.Message):
-    tts = gTTS("Hello, this is a voice message from your bot", lang='en')
-    tts.save("voice.mp3")
-    voice = InputFile("voice.mp3")
-    await bot.send_voice(message.chat.id, voice)
-    os.remove("voice.mp3")
+    tts = gTTS("Привет, это голосовое сообщение от вашего бота", lang='ru')
 
-@dp.message(content_types=['text'])
-async def translate_text(message: types.Message):
-    translated = translator.translate(message.text, src='auto', dest='en')
-    await message.reply(translated.text)
+    file_path = "voice1.ogg"
+
+    # Сохранение файла
+    tts.save(file_path)
+
+    # Отправка файла
+    voice = FSInputFile(file_path)
+    await bot.send_voice(message.chat.id, voice)
+
+    # Удаление файла
+    os.remove(file_path)
+
+@dp.message(Command(commands=['text']))
+async def request_photo(message: Message):
+    await message.answer("Пожалуйста, отправьте мне сообщение для перевода")
+
+
+# Обработчик для текстовых сообщений
+@dp.message(lambda message: message.content_type == 'text')
+async def translate_text(message: Message):
+    try:
+        translated = translator.translate(message.text, src='auto', dest='en')
+        await message.reply(translated.text)
+    except Exception as e:
+        await message.reply(f"Error: {e}")
+        print(f"Ошибка при переводе: {message.text} - {e}")
+
 
 # Запуск бота
 async def main():
