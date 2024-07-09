a0c0b821bee13f18d10b2a80ca0206fda640fb8f
Olga-Pro <oip_@inbox.ru>
2024-07-09 11:53:42 +0300

    tg06

--- hw_tg06.py
+++ hw_tg06.py
@@ -4,6 +4,8 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
 
 
 import keyb_tg06 as kb
@@ -28,7 +30,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 # Создаем БД для хранения данных о пользователях и расходах
-conn = sqlite3.connect('user.db')
+conn = sqlite3.connect('users_tg06.db')
 cursor = conn.cursor()
 cursor.execute(''' CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
@@ -97,14 +99,94 @@ async def exchange_rates(message: Message):
 
 @dp.message(F.text == "Советы по экономии")
 async def send_tips(message: Message):
-    tips = {
+    tips = [
         "Совет 1: Ведите бюджет и следите за своими расходами.",
         "Совет 2: Откладывайте часть доходов на сбережения.",
         "Совет 3: Покупайте товары по скидкам и распродажам."
-            }
+            ]
     tip = random.choice(tips)
     await message.answer(tip)
 
+
+@dp.message(F.text == "Личные финансы")
+async def finances(message: Message, state: FSMContext):
+    await state.set_state(FinancesForm.category1)
+    await message.reply("Введите первую категорию расходов")
+
+
+@dp.message(FinancesForm.category1)
+async def finances(message: Message, state: FSMContext):
+    await state.update_data(category1 = message.text)
+    await state.set_state(FinancesForm.expenses1)
+    await message.reply("Введите расходы для категории 1:")
+
+
+@dp.message(FinancesForm.expenses1)
+async def finances(message: Message, state: FSMContext):
+    await state.update_data(expenses1 = float(message.text))
+    await state.set_state(FinancesForm.category2)
+    await message.reply("Введите вторую категорию расходов:")
+
+@dp.message(FinancesForm.category2)
+async def finances(message: Message, state: FSMContext):
+    await state.update_data(category2 = message.text)
+    await state.set_state(FinancesForm.expenses2)
+    await message.reply("Введите расходы для категории 2:")
+
+
+@dp.message(FinancesForm.expenses2)
+async def finances(message: Message, state: FSMContext):
+    await state.update_data(expenses2 = float(message.text))
+    await state.set_state(FinancesForm.category3)
+    await message.reply("Введите третью категорию расходов:")
+
+@dp.message(FinancesForm.category3)
+async def finances(message: Message, state: FSMContext):
+    await state.update_data(category3 = message.text)
+    await state.set_state(FinancesForm.expenses3)
+    await message.reply("Введите расходы для категории 3:")
+
+
+@dp.message(FinancesForm.expenses3)
+async def finances(message: Message, state: FSMContext):
+    data = await state.get_data()
+    telegram_id = message.from_user.id
+    cursor.execute('''
+    UPDATE users SET category1 = ?, expenses1 = ?, category2 = ?, expenses2 = ?, category3 = ?, expenses3 = ?
+    WHERE telegram_id = ?''',
+                   (data['category1'], data['expenses1'],data['category2'], data['expenses2'],
+                    data['category3'], float(message.text),telegram_id)
+                   )
+    conn.commit()
+    await state.clear()
+
+    await message.answer("Категории и расходы сохранены!")
+
+
+@dp.message(Command(commands=['base']))
+async def show_base(message: Message):
+    # Извлечение всех данных из базы данных
+    conn = sqlite3.connect('users_tg06.db')
+    cur = conn.cursor()
+    cur.execute('''SELECT id, telegram_id, name,  
+                category1, category2, category3, 
+                expenses1, expenses2, expenses3 FROM users''')
+    rows = cur.fetchall()
+    conn.close()
+
+    # Формирование сообщения с данными
+    if rows:
+        response = "Содержимое таблицы БД:\n\n"
+        for row in rows:
+            response += f"ID: {row[0]}, TG ID: {row[1]}, Имя: {row[2]}\n"
+            response += f"{row[3]} - {row[6]}\n"
+            response += f"{row[4]} - {row[7]}\n"
+            response += f"{row[5]} - {row[8]}\n\n"
+    else:
+        response = "В базе данных нет данных."
+
+    # Отправка сообщения пользователю
+    await message.answer(response)
 # Запуск бота
 async def main():
     await dp.start_polling(bot)
