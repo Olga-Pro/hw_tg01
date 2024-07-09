4fe4959db26c42386de02494c77c175ce8f07d6c
Olga-Pro <oip_@inbox.ru>
2024-07-09 10:45:49 +0300

    create tg06

---
+++ keyb_tg06.py
@@ -0,0 +1,13 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+
+button_register = KeyboardButton(text="Регистрация в ТГ-боте")
+button_exchange_rates = KeyboardButton(text="Курс валют")
+button_tips = KeyboardButton(text="Советы по экономии")
+button_finances = KeyboardButton(text="Личные финансы")
+
+keyboards = ReplyKeyboardMarkup(keyboard=[
+    [button_register, button_exchange_rates],
+    [button_tips, button_finances]
+    ], resize_keyboard=True)
